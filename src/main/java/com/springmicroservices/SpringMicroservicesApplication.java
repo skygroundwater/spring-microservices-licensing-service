98aaa9fa6c97e6c9aef847d9e96625c58a44ed30
skygroundwater <o.metelev2020@yande.ru>
2023-12-07 07:48:25 +0300

    Start doing the project for spring-microservices. licensing-service for example

---
+++ src/main/java/com/springmicroservices/SpringMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.springmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicesApplication.class, args);
+    }
+}
